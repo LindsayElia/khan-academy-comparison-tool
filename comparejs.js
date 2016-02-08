9592e7e83e43e9a3c26f16fc7d4f962a94a6cc74
LindsayElia <lindsay.elia@gmail.com>
2016-02-07 17:33:31 -0800

    initial commit, starting project

---
+++ comparejs.js
@@ -0,0 +1,25 @@
+// global variables
+var $anything;
+var $firstVariable;
+var $secondVariable;
+
+
+// MUST HAVE
+// function to check that input program DOES have a variable declaration
+// "var = x;"
+
+// MUST HAVE
+// function to check that input program DOES have a for loop
+
+
+// MUST NOT HAVE
+// function to check that input program DOES NOT have a while loop
+
+// MUST NOT HAVE
+// function to check that input program DOES NOT have an if statement
+
+
+// STRUCTURE
+// function to check that there is a for loop with an if statement contained inside of the for loop
+
+
